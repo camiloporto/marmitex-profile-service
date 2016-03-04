130574fc9f0efd8c2e0d95bba8affdedf0993876
ur42 <ur42@petrobras.com.br>
2016-03-04 17:05:47 -0300

    reorganizado classes e pacotes do servico ProfileRest

--- src/test/java/br/com/camiloporto/marmitex/microservice/profile/rest/ProfileRestTest.java
+++ src/test/java/br/com/camiloporto/marmitex/microservice/profile/rest/ProfileRestTest.java
@@ -1,19 +1,16 @@
 package br.com.camiloporto.marmitex.microservice.profile.rest;
 
-import br.com.camiloporto.marmitex.ServiceApplication;
+import br.com.camiloporto.marmitex.microservice.profile.ServiceApplication;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockServletContext;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.hamcrest.core.Is.is;
