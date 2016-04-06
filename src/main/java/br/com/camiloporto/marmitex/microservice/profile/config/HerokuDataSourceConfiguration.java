8d9df203d232be45122902182fbb16dc5469ff62
ur42 <ur42@petrobras.com.br>
2016-04-06 08:24:10 -0300

    fixed datasource configuration for heroku environment

--- src/main/java/br/com/camiloporto/marmitex/microservice/profile/config/HerokuDataSourceConfiguration.java
+++ src/main/java/br/com/camiloporto/marmitex/microservice/profile/config/HerokuDataSourceConfiguration.java
@@ -1,6 +1,7 @@
 package br.com.camiloporto.marmitex.microservice.profile.config;
 
 import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
@@ -15,12 +16,14 @@ public class HerokuDataSourceConfiguration implements DataSourceConfiguration {
 
 
     @Override
+    @Bean
     public DataSource createDataSource() {
         String dbUrl = System.getenv("JDBC_DATABASE_URL");
         String username = System.getenv("JDBC_DATABASE_USERNAME");
         String password = System.getenv("JDBC_DATABASE_PASSWORD");
 
         HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setDataSourceClassName("org.postgresql.Driver");
         dataSource.setJdbcUrl(dbUrl);
         dataSource.setUsername(username);
         dataSource.setPassword(password);
